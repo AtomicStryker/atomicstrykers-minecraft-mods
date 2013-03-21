9769d35ce10b23c480932898d0c2eef401588be4
atomicstryker <atomicstryker@localhost>
2013-03-21 14:23:34 +0100

    Magic Yarn 1.5.1 update

--- MagicYarn/common/atomicstryker/magicyarn/common/ItemMagicYarn.java
+++ MagicYarn/common/atomicstryker/magicyarn/common/ItemMagicYarn.java
@@ -21,9 +21,9 @@ public class ItemMagicYarn extends Item
 	}
 	
 	@Override
-    public void func_94581_a(IconRegister iconRegister)
+    public void updateIcons(IconRegister iconRegister)
     {
-        iconIndex = iconRegister.func_94245_a("magicyarn:magicYarn");
+        iconIndex = iconRegister.registerIcon("magicyarn:magicYarn");
     }
 
 	@Override
