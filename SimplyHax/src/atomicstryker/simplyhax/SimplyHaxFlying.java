43486b37aa545a53510d13409fdc283078497d63
atomicstryker <atomicstryker@localhost>
2013-07-10 19:59:41 +0200

    Simply Hax 1.6.2

--- SimplyHax/src/atomicstryker/simplyhax/SimplyHaxFlying.java
+++ SimplyHax/src/atomicstryker/simplyhax/SimplyHaxFlying.java
@@ -29,7 +29,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 
-@Mod(modid = "SimplyHaxFlying", name = "Simply Hax Flying", version = "1.6.1")
+@Mod(modid = "SimplyHaxFlying", name = "Simply Hax Flying", version = "1.6.2")
 public class SimplyHaxFlying
 {
     private long lastTime;
