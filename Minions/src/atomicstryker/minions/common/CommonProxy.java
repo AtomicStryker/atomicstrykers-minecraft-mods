50681b95aebbd6ab0aed46706bb83ebe22e3368d
atomicstryker <atomicstryker@localhost>
2013-07-20 18:28:00 +0200

    Minions 1.6.9

--- Minions/src/atomicstryker/minions/common/CommonProxy.java
+++ Minions/src/atomicstryker/minions/common/CommonProxy.java
@@ -34,7 +34,11 @@ public class CommonProxy
     public void sendSoundToClients(Entity ent, String string)
     {
         Object[] toSend = {ent.entityId, string};
-        FMLCommonHandler.instance().getMinecraftServerInstance().getConfigurationManager().sendToAllNear(ent.posX, ent.posY, ent.posZ, 15, ent.worldObj.provider.dimensionId, ForgePacketWrapper.createPacket(MinionsCore.getPacketChannel(), PacketType.SOUNDTOALL.ordinal(), toSend));
+        try
+        {
+            FMLCommonHandler.instance().getMinecraftServerInstance().getConfigurationManager().sendToAllNear(ent.posX, ent.posY, ent.posZ, 15, ent.worldObj.provider.dimensionId, ForgePacketWrapper.createPacket(MinionsCore.getPacketChannel(), PacketType.SOUNDTOALL.ordinal(), toSend));
+        }
+        catch (Exception e){}
     }
 
     public void OnMastersGloveRightClickHeld(ItemStack itemstack, World world, EntityPlayer player)
