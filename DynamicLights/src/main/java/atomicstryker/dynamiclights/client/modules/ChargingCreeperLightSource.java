fdba1eb74497025cf9c0649239c0e5d68197932e
atomicstryker <atomicstryker@localhost>
2014-02-20 00:27:09 +0100

    Small change to Creeper module in Dynamic Lights, now switches off when Creeper doesnt explode after all

--- DynamicLights/src/main/java/atomicstryker/dynamiclights/client/modules/ChargingCreeperLightSource.java
+++ DynamicLights/src/main/java/atomicstryker/dynamiclights/client/modules/ChargingCreeperLightSource.java
@@ -14,77 +14,42 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 /**
  * 
  * @author AtomicStryker
- *
- * Offers Dynamic Light functionality to charging Creepers about to explode.
- * Those can give off Light through this Module.
- *
+ * 
+ *         Offers Dynamic Light functionality to charging Creepers about to
+ *         explode. Those can give off Light through this Module.
+ * 
  */
-@Mod(modid = "DynamicLights_creepers", name = "Dynamic Lights on Creepers", version = "1.0.3", dependencies = "required-after:DynamicLights")
+@Mod(modid = "DynamicLights_creepers", name = "Dynamic Lights on Creepers", version = "1.0.4", dependencies = "required-after:DynamicLights")
 public class ChargingCreeperLightSource
 {
-    
+
     @EventHandler
     public void load(FMLInitializationEvent evt)
     {
         MinecraftForge.EVENT_BUS.register(this);
     }
-    
+
     @SubscribeEvent
     public void onPlaySoundAtEntity(PlaySoundAtEntityEvent event)
     {
-        if (event.name != null
-        && event.name.equals("random.fuse")
-        && event.entity != null
-        && event.entity instanceof EntityCreeper)
+        if (event.name != null && event.name.equals("random.fuse") && event.entity != null && event.entity instanceof EntityCreeper)
         {
             if (event.entity.isEntityAlive())
             {
-                EntityLightAdapter adapter = new EntityLightAdapter((EntityCreeper) event.entity);
-                adapter.onTick();
+                DynamicLights.addLightSource(new EntityLightAdapter((EntityCreeper) event.entity));
             }
         }
     }
-    
+
     private class EntityLightAdapter implements IDynamicLightSource
     {
-        
         private EntityCreeper entity;
-        private int lightLevel;
-        private boolean enabled;
-        
+
         public EntityLightAdapter(EntityCreeper eC)
         {
-            lightLevel = 15;
-            enabled = false;
             entity = eC;
         }
-        
-        public void onTick()
-        {
-            lightLevel = entity.getCreeperState() == 1 ? 15 : 0;
-            
-            if (!enabled && lightLevel > 0)
-            {
-                enableLight();
-            }
-            else if (enabled && lightLevel < 1)
-            {
-                disableLight();
-            }
-        }
-        
-        private void enableLight()
-        {
-            DynamicLights.addLightSource(this);
-            enabled = true;
-        }
-        
-        private void disableLight()
-        {
-            DynamicLights.removeLightSource(this);
-            enabled = false;
-        }
-     
+
         @Override
         public Entity getAttachmentEntity()
         {
@@ -94,7 +59,7 @@ public class ChargingCreeperLightSource
         @Override
         public int getLightLevel()
         {
-            return lightLevel;
+            return entity.getCreeperState() == 1 ? 15 : 0;
         }
     }
 
