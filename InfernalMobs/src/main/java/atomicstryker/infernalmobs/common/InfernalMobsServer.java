f9786109be15bd429d4342163e0d9dcdc6b3fc18
atomicstryker <atomicstryker@localhost>
2014-05-13 15:41:45 +0200

    Infernal Mobs 1.5.1

--- InfernalMobs/src/main/java/atomicstryker/infernalmobs/common/InfernalMobsServer.java
+++ InfernalMobs/src/main/java/atomicstryker/infernalmobs/common/InfernalMobsServer.java
@@ -3,10 +3,6 @@ package atomicstryker.infernalmobs.common;
 import java.util.concurrent.ConcurrentHashMap;
 
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
-import atomicstryker.infernalmobs.common.network.HealthPacket;
-import atomicstryker.infernalmobs.common.network.MobModsPacket;
 
 public class InfernalMobsServer implements ISidedProxy
 {
@@ -37,23 +33,9 @@ public class InfernalMobsServer implements ISidedProxy
     }
 
     @Override
-    public void onHealthPacket(String stringData, int entID, float health, float maxhealth)
+    public void onHealthPacketForClient(String stringData, int entID, float health, float maxhealth)
     {
-        EntityPlayerMP p = MinecraftServer.getServer().getConfigurationManager().getPlayerForUsername(stringData);
-        if (p != null)
-        {
-            EntityLivingBase e = (EntityLivingBase) p.worldObj.getEntityByID(entID);
-            if (e != null)
-            {
-                MobModifier mod = InfernalMobsCore.getMobModifiers(e);
-                if (mod != null)
-                {
-                    health = e.getHealth();
-                    maxhealth = e.getMaxHealth();
-                    InfernalMobsCore.instance().networkHelper.sendPacketToPlayer(new HealthPacket(stringData, entID, health, maxhealth), p);
-                }
-            }
-        }
+        // NOOP
     }
 
     @Override
@@ -63,23 +45,9 @@ public class InfernalMobsServer implements ISidedProxy
     }
 
     @Override
-    public void onMobModsPacket(String stringData, int entID)
+    public void onMobModsPacketToClient(String stringData, int entID)
     {
-        EntityPlayerMP p = MinecraftServer.getServer().getConfigurationManager().getPlayerForUsername(stringData);
-        if (p != null)
-        {
-            EntityLivingBase e = (EntityLivingBase) p.worldObj.getEntityByID(entID);
-            if (e != null)
-            {
-                MobModifier mod = InfernalMobsCore.getMobModifiers(e);
-                if (mod != null)
-                {
-                    stringData = mod.getLinkedModNameUntranslated();
-                    InfernalMobsCore.instance().networkHelper.sendPacketToPlayer(new MobModsPacket(stringData, entID), p);
-                    InfernalMobsCore.instance().sendHealthPacket(e, mod.getActualHealth(e));
-                }
-            }
-        }
+        // NOOP
     }
 
     @Override
