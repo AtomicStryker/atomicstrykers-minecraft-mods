206bd51f7f5127f39563c038dcb865e473ff6b1e
atomicstryker <atomicstryker@localhost>
2014-08-01 22:39:22 +0200

    Ruins 13.6

--- Ruins/src/main/java/atomicstryker/ruins/common/EventRuinTemplateSpawn.java
+++ Ruins/src/main/java/atomicstryker/ruins/common/EventRuinTemplateSpawn.java
@@ -5,16 +5,18 @@ import net.minecraft.world.World;
 import net.minecraftforge.event.world.WorldEvent;
 
 /**
- * Executed before a Ruins template is created in the world by the Worldgen Hook.
+ * Executed before and after a Ruins template is created in the world by the Worldgen Hook.
  * Does also fire before manually spawning a template with /testruin.
+ * Note: Only the isPrior=false event has the final y value for the boundaries after embedding
  */
 @Cancelable
 public class EventRuinTemplateSpawn extends WorldEvent
 {
     
-    final RuinTemplate template;
-    final int x, y, z, rotation;
-    final boolean testingRuin;
+    public final RuinTemplate template;
+    public final int x, y, z, rotation;
+    public final boolean testingRuin;
+    public final boolean isPrePhase;
     
     /**
      * @param world World template is about to spawn in
@@ -23,9 +25,10 @@ public class EventRuinTemplateSpawn extends WorldEvent
      * @param b y Coordinate
      * @param c z Coordinate
      * @param r Rotation value
-     * @param bool whether or not the template was manually spawned
+     * @param testing whether or not the template was manually spawned
+     * @param isPrior true before a Ruins spawned, can be cancelled only then, false otherwise
      */
-    public EventRuinTemplateSpawn(World world, RuinTemplate templ, int a, int b, int c, int r, boolean bool)
+    public EventRuinTemplateSpawn(World world, RuinTemplate templ, int a, int b, int c, int r, boolean testing, boolean isPrior)
     {
         super(world);
         template = templ;
@@ -33,7 +36,8 @@ public class EventRuinTemplateSpawn extends WorldEvent
         y = b;
         z = c;
         rotation = r;
-        testingRuin = bool;
+        testingRuin = testing;
+        isPrePhase = isPrior;
     }
 
 }
