49b9df34588e2a02b2f0a9e8e0b42fa33ea0daca
AtomicStryker <atomic_662003@yahoo.co.uk>
2015-08-05 15:38:21 +0200

    Debug logging hotfix for Ruins

--- Ruins/src/main/java/atomicstryker/ruins/common/RuinTemplateRule.java
+++ Ruins/src/main/java/atomicstryker/ruins/common/RuinTemplateRule.java
@@ -69,6 +69,10 @@ public class RuinTemplateRule
             int bracketCounter = 1;
             for (;;closingIndex++)
             {
+                if (closingIndex == rule.length())
+                {
+                    throw new Exception("Unbalanced brackets in Ruins template "+r+", offending rule: "+rule);
+                }
                 if (rule.charAt(closingIndex) == '{')
                 {
                     bracketCounter++;
