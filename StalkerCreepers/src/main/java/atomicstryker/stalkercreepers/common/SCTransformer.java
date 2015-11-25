16910e9e88440de7d833ac354427b90d75e5a94d
atomicstryker <atomic_662003@yahoo.co.uk>
2015-11-25 17:26:32 +0100

    ports stalker creepers to 188

--- StalkerCreepers/src/main/java/atomicstryker/stalkercreepers/common/SCTransformer.java
+++ StalkerCreepers/src/main/java/atomicstryker/stalkercreepers/common/SCTransformer.java
@@ -1,20 +1,11 @@
 package atomicstryker.stalkercreepers.common;
 
-import static org.objectweb.asm.Opcodes.ALOAD;
-import static org.objectweb.asm.Opcodes.GETFIELD;
-import static org.objectweb.asm.Opcodes.ICONST_0;
-import static org.objectweb.asm.Opcodes.ICONST_1;
-import static org.objectweb.asm.Opcodes.IFNE;
-import static org.objectweb.asm.Opcodes.INVOKESTATIC;
-import static org.objectweb.asm.Opcodes.IRETURN;
-
 import java.util.Iterator;
-
-import net.minecraft.launchwrapper.IClassTransformer;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Label;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldInsnNode;
@@ -26,6 +17,8 @@ import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.VarInsnNode;
 
+import net.minecraft.launchwrapper.IClassTransformer;
+
 
 
 public class SCTransformer implements IClassTransformer
@@ -33,12 +26,12 @@ public class SCTransformer implements IClassTransformer
     /* Obfuscated Names for EntityAICreeperSwell Transformation */
     
     /* class net.minecraft.src.EntityAICreeperSwell */
-    private final String entityAICreeperSwellClassNameO = "aae";
-    private final String entityAICreeperSwellJavaClassNameO = "aae";
+    private final String entityAICreeperSwellClassNameO = "sf";
+    private final String entityAICreeperSwellJavaClassNameO = "sf";
     /* class net.minecraft.src.EntityLiving */
-    private final String entityLivingJavaClassNameO = "xn";
+    private final String entityLivingJavaClassNameO = "ps";
     /* class net.minecraft.src.EntityCreeper */
-    private final String entityCreeperJavaClassNameO = "aep";
+    private final String entityCreeperJavaClassNameO = "vn";
     /*shouldExecute() / func_75250_a */
     private final String shouldExecuteMethodNameO = "a";
     /*swellingCreeper / field_75269_a */
@@ -90,7 +83,7 @@ public class SCTransformer implements IClassTransformer
                 for (int index = 0; index < m.instructions.size(); index++)
                 {
                     AbstractInsnNode curNode = m.instructions.get(index);
-                    if (curNode.getOpcode() == ICONST_1)
+                    if (curNode.getOpcode() == Opcodes.ICONST_1)
                     {
                         nodeTarget = curNode;
                     }
@@ -108,21 +101,21 @@ public class SCTransformer implements IClassTransformer
                 // make an exit label node
                 LabelNode exitLabelNode = new LabelNode(new Label());
                                
-                toInject.add(new VarInsnNode(ALOAD, 0));
-                toInject.add(new FieldInsnNode(GETFIELD, entityAICreeperSwellJavaClassNameO, swellingCreeperFieldNameO, "L"+entityCreeperJavaClassNameO+";"));
+                toInject.add(new VarInsnNode(Opcodes.ALOAD, 0));
+                toInject.add(new FieldInsnNode(Opcodes.GETFIELD, entityAICreeperSwellJavaClassNameO, swellingCreeperFieldNameO, "L"+entityCreeperJavaClassNameO+";"));
                 
                 try
                 {
                     try
                     {
                         AbstractInsnNode node = MethodInsnNode.class.getConstructor(int.class, String.class, String.class, String.class).newInstance(
-                                INVOKESTATIC, "atomicstryker/stalkercreepers/common/EntityAIHelperStalker", "isSeenByTarget", "(L"+entityLivingJavaClassNameO+";)Z");
+                        		Opcodes.INVOKESTATIC, "atomicstryker/stalkercreepers/common/EntityAIHelperStalker", "isSeenByTarget", "(L"+entityLivingJavaClassNameO+";)Z");
                         toInject.add(node);
                     }
                     catch (NoSuchMethodException e)
                     {
                         AbstractInsnNode node = MethodInsnNode.class.getConstructor(int.class, String.class, String.class, String.class, boolean.class).newInstance(
-                                INVOKESTATIC, "atomicstryker/stalkercreepers/common/EntityAIHelperStalker", "isSeenByTarget", "(L"+entityLivingJavaClassNameO+";)Z", false);
+                        		Opcodes.INVOKESTATIC, "atomicstryker/stalkercreepers/common/EntityAIHelperStalker", "isSeenByTarget", "(L"+entityLivingJavaClassNameO+";)Z", false);
                         toInject.add(node);
                     }
                 }
@@ -133,9 +126,9 @@ public class SCTransformer implements IClassTransformer
                     return bytes;
                 }
                 
-                toInject.add(new JumpInsnNode(IFNE, exitLabelNode));
-                toInject.add(new InsnNode(ICONST_0));
-                toInject.add(new InsnNode(IRETURN));
+                toInject.add(new JumpInsnNode(Opcodes.IFNE, exitLabelNode));
+                toInject.add(new InsnNode(Opcodes.ICONST_0));
+                toInject.add(new InsnNode(Opcodes.IRETURN));
                 toInject.add(exitLabelNode);
                 
                 // inject new instruction list into method instruction list
@@ -172,7 +165,7 @@ public class SCTransformer implements IClassTransformer
                 for (int index = 0; index < m.instructions.size(); index++)
                 {
                     AbstractInsnNode curNode = m.instructions.get(index);
-                    if (curNode.getOpcode() == ICONST_1)
+                    if (curNode.getOpcode() == Opcodes.ICONST_1)
                     {
                         nodeTarget = curNode;
                     }
@@ -190,21 +183,21 @@ public class SCTransformer implements IClassTransformer
                 // make an exit label node
                 LabelNode exitLabelNode = new LabelNode(new Label());
                                 
-                toInject.add(new VarInsnNode(ALOAD, 0));
-                toInject.add(new FieldInsnNode(GETFIELD, entityAICreeperSwellJavaClassName, swellingCreeperFieldName, "L"+entityCreeperJavaClassName+";"));
+                toInject.add(new VarInsnNode(Opcodes.ALOAD, 0));
+                toInject.add(new FieldInsnNode(Opcodes.GETFIELD, entityAICreeperSwellJavaClassName, swellingCreeperFieldName, "L"+entityCreeperJavaClassName+";"));
                 
                 try
                 {
                     try
                     {
                         AbstractInsnNode node = MethodInsnNode.class.getConstructor(int.class, String.class, String.class, String.class).newInstance(
-                                INVOKESTATIC, "atomicstryker/stalkercreepers/common/EntityAIHelperStalker", "isSeenByTarget", "(L"+entityLivingJavaClassName+";)Z");
+                        		Opcodes.INVOKESTATIC, "atomicstryker/stalkercreepers/common/EntityAIHelperStalker", "isSeenByTarget", "(L"+entityLivingJavaClassName+";)Z");
                         toInject.add(node);
                     }
                     catch (NoSuchMethodException e)
                     {
                         AbstractInsnNode node = MethodInsnNode.class.getConstructor(int.class, String.class, String.class, String.class, boolean.class).newInstance(
-                                INVOKESTATIC, "atomicstryker/stalkercreepers/common/EntityAIHelperStalker", "isSeenByTarget", "(L"+entityLivingJavaClassName+";)Z", false);
+                        		Opcodes.INVOKESTATIC, "atomicstryker/stalkercreepers/common/EntityAIHelperStalker", "isSeenByTarget", "(L"+entityLivingJavaClassName+";)Z", false);
                         toInject.add(node);
                     }
                 }
@@ -215,9 +208,9 @@ public class SCTransformer implements IClassTransformer
                     return bytes;
                 }
                 
-                toInject.add(new JumpInsnNode(IFNE, exitLabelNode));
-                toInject.add(new InsnNode(ICONST_0));
-                toInject.add(new InsnNode(IRETURN));
+                toInject.add(new JumpInsnNode(Opcodes.IFNE, exitLabelNode));
+                toInject.add(new InsnNode(Opcodes.ICONST_0));
+                toInject.add(new InsnNode(Opcodes.IRETURN));
                 toInject.add(exitLabelNode);
                 
                 // inject new instruction list into method instruction list
