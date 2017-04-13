691b1c47c79afdd7235190b6c4c87acc4330d5d9
atomicstryker <atomic_662003@yahoo.co.uk>
2017-04-13 14:11:19 +0200

    Removing some debugspam in Pet Bat

--- PetBat/src/main/java/atomicstryker/petbat/common/PetBatMod.java
+++ PetBat/src/main/java/atomicstryker/petbat/common/PetBatMod.java
@@ -300,11 +300,13 @@ public class PetBatMod implements IProxy
     @SubscribeEvent
     public void onItemToss(ItemTossEvent event)
     {
-        System.out.println("onItemTossEvent hooked, isRemote: " + event.getEntity().worldObj.isRemote);
+        // System.out.println("onItemTossEvent hooked, isRemote: " +
+        // event.getEntity().worldObj.isRemote);
         if (!event.getEntity().worldObj.isRemote)
         {
             EntityItem itemDropped = event.getEntityItem();
-            System.out.println("PlayerDropsEvent iterating over drop " + itemDropped);
+            // System.out.println("PlayerDropsEvent iterating over drop " +
+            // itemDropped);
             EntityItem foundItem;
             final Item id = itemDropped.getEntityItem().getItem();
             if (id == itemPocketedBat)
