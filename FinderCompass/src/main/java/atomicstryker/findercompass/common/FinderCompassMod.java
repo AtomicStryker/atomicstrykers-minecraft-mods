78b89e7d7ca06fa4150a4a2f1603993a138665fc
atomicstryker <atomic_662003@yahoo.co.uk>
2017-06-15 20:11:35 +0200

    Finder Compass disabled the compass recipe for now

--- FinderCompass/src/main/java/atomicstryker/findercompass/common/FinderCompassMod.java
+++ FinderCompass/src/main/java/atomicstryker/findercompass/common/FinderCompassMod.java
@@ -12,15 +12,9 @@ import atomicstryker.findercompass.client.FinderCompassClientTicker;
 import atomicstryker.findercompass.common.network.HandshakePacket;
 import atomicstryker.findercompass.common.network.NetworkHelper;
 import atomicstryker.findercompass.common.network.StrongholdPacket;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.CraftingManager;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.item.crafting.ShapedRecipes;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
@@ -36,111 +30,105 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.fml.common.network.NetworkCheckHandler;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
-@Mod(modid = "findercompass", name = "Finder Compass", version = "1.11.2")
-public class FinderCompassMod {
+@Mod(modid = "findercompass", name = "Finder Compass", version = "1.12")
+public class FinderCompassMod
+{
 
-	@Instance(value = "findercompass")
-	public static FinderCompassMod instance;
+    @Instance(value = "findercompass")
+    public static FinderCompassMod instance;
 
-	public ArrayList<CompassSetting> settingList;
+    public ArrayList<CompassSetting> settingList;
 
-	public ItemFinderCompass compass;
-	public boolean itemEnabled;
+    public static ItemFinderCompass compass;
+    public static boolean itemEnabled;
 
-	public File compassConfig;
+    public File compassConfig;
 
-	public NetworkHelper networkHelper;
+    public NetworkHelper networkHelper;
 
-	@NetworkCheckHandler
-	public boolean checkModLists(Map<String, String> modList, Side side) {
-		return true;
-	}
-
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent evt) {
-		settingList = new ArrayList<>();
-
-		compassConfig = evt.getSuggestedConfigurationFile();
-		Configuration itemConfig = new Configuration(
-				new File(compassConfig.getAbsolutePath().replace("findercompass", "FinderCompassItemConfig")));
-		System.out.println("Finder compass needle config location: " + compassConfig.getAbsolutePath());
-		System.out.println("Finder compass item config location: " + itemConfig.getConfigFile().getAbsolutePath());
-		itemConfig.load();
-		itemEnabled = itemConfig.get(Configuration.CATEGORY_GENERAL, "isFinderCompassNewItem", false).getBoolean(false);
-		itemConfig.save();
-
-		if (itemEnabled) {
-			compass = (ItemFinderCompass) new ItemFinderCompass().setUnlocalizedName("finder_compass")
-					.setRegistryName("findercompass", "finder_compass");
-			GameRegistry.register(compass);
-		}
-
-		MinecraftForge.EVENT_BUS.register(this);
-
-		if (FMLCommonHandler.instance().getEffectiveSide().isClient()) {
-			FinderCompassClientTicker.instance = new FinderCompassClientTicker();
-		}
-
-		networkHelper = new NetworkHelper("AS_FC", HandshakePacket.class, StrongholdPacket.class);
-	}
-
-	@SubscribeEvent
-	public void onPlayerLogin(PlayerLoggedInEvent event) {
-		networkHelper.sendPacketToPlayer(new HandshakePacket("server"), (EntityPlayerMP) event.player);
-	}
-
-	@EventHandler
-	public void load(FMLInitializationEvent evt) {
-		if (itemEnabled) {
-			// GameRegistry.addRecipe(new ItemStack(compass)," # ", "#X#", " #
-			// ", '#', Items.DIAMOND, 'X', Items.COMPASS);
-			Ingredient none = Ingredient.field_193370_a;
-			Ingredient diamond = fromItem(Items.DIAMOND);
-			Ingredient Ingredientcompass = fromItem(Items.COMPASS);
-			ResourceLocation rl = new ResourceLocation("findercompass", "findercompass");
-			CraftingManager.func_193372_a(rl, new ShapedRecipes(rl.toString(), 3, 3,
-					NonNullList.func_193580_a(
-							// start with placeholder, because resizable nonnull-list
-							none,
-							none, diamond, none,
-							diamond, Ingredientcompass, diamond,
-							none, diamond, none),
-					new ItemStack(compass)));
-		}
-
-		if (FMLCommonHandler.instance().getEffectiveSide().isClient()) {
-			FinderCompassClientTicker.instance.onLoad();
-		}
-	}
-	
-	// courtesy of gigaherz
-    private Ingredient fromItem(Item item)
+    @NetworkCheckHandler
+    public boolean checkModLists(Map<String, String> modList, Side side)
     {
-        if (!item.getHasSubtypes())
-            return Ingredient.func_193369_a(new ItemStack(item));
-
-        NonNullList<ItemStack> stacks = NonNullList.create();
-        item.getSubItems(CreativeTabs.SEARCH, stacks);
-        return Ingredient.func_193369_a(stacks.toArray(new ItemStack[stacks.size()]));
+        return true;
     }
 
-	@EventHandler
-	public void onModsLoaded(FMLPostInitializationEvent event) {
-		DefaultConfigFilePrinter configurator = new DefaultConfigFilePrinter();
-		if (!compassConfig.exists()) {
-			configurator.writeDefaultFile(compassConfig);
-		}
-		try {
-			configurator.parseConfig(new BufferedReader(new FileReader(compassConfig)), settingList);
-			System.out.println("Finder compass config fully parsed, loaded " + settingList.size() + " settings");
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent evt)
+    {
+        settingList = new ArrayList<>();
 
-			if (FMLCommonHandler.instance().getEffectiveSide().isClient()) {
-				FinderCompassClientTicker.instance.switchSetting();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+        compassConfig = evt.getSuggestedConfigurationFile();
+        Configuration itemConfig = new Configuration(new File(compassConfig.getAbsolutePath().replace("findercompass", "FinderCompassItemConfig")));
+        System.out.println("Finder compass needle config location: " + compassConfig.getAbsolutePath());
+        System.out.println("Finder compass item config location: " + itemConfig.getConfigFile().getAbsolutePath());
+        itemConfig.load();
+        itemEnabled = itemConfig.get(Configuration.CATEGORY_GENERAL, "isFinderCompassNewItem", false).getBoolean(false);
+        itemConfig.save();
+
+        if (itemEnabled)
+        {
+            compass = (ItemFinderCompass) new ItemFinderCompass().setUnlocalizedName("finder_compass").setRegistryName("findercompass", "finder_compass");
+            GameRegistry.register(compass);
+        }
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        if (FMLCommonHandler.instance().getEffectiveSide().isClient())
+        {
+            FinderCompassClientTicker.instance = new FinderCompassClientTicker();
+        }
+
+        networkHelper = new NetworkHelper("AS_FC", HandshakePacket.class, StrongholdPacket.class);
+    }
+
+    @SubscribeEvent
+    public void onPlayerLogin(PlayerLoggedInEvent event)
+    {
+        networkHelper.sendPacketToPlayer(new HandshakePacket("server"), (EntityPlayerMP) event.player);
+    }
+
+    @EventHandler
+    public void load(FMLInitializationEvent evt)
+    {
+
+        if (itemEnabled)
+        {
+            ResourceLocation rl = new ResourceLocation("findercompass", "findercompass");
+            ShapedOreRecipe recipe = new ShapedOreRecipe(rl, new ItemStack(compass), " # ", "#X#", " # ", '#', Items.DIAMOND, 'X', Items.COMPASS);
+            // disabled until forge works
+            // GameRegistry.register(recipe);
+        }
+
+        if (FMLCommonHandler.instance().getEffectiveSide().isClient())
+        {
+            FinderCompassClientTicker.instance.onLoad();
+        }
+    }
+
+    @EventHandler
+    public void onModsLoaded(FMLPostInitializationEvent event)
+    {
+        DefaultConfigFilePrinter configurator = new DefaultConfigFilePrinter();
+        if (!compassConfig.exists())
+        {
+            configurator.writeDefaultFile(compassConfig);
+        }
+        try
+        {
+            configurator.parseConfig(new BufferedReader(new FileReader(compassConfig)), settingList);
+            System.out.println("Finder compass config fully parsed, loaded " + settingList.size() + " settings");
+
+            if (FMLCommonHandler.instance().getEffectiveSide().isClient())
+            {
+                FinderCompassClientTicker.instance.switchSetting();
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
 }
