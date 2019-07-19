ff0639c11781f5652794d9206ed760427a553369
atomicstryker <atomic_662003@yahoo.co.uk>
2019-07-19 07:20:34 +0200

    Finder compass 1.13.2.2

--- FinderCompass/src/main/java/atomicstryker/findercompass/common/FinderCompassMod.java
+++ FinderCompass/src/main/java/atomicstryker/findercompass/common/FinderCompassMod.java
@@ -58,7 +58,7 @@ public class FinderCompassMod {
     public void preInit(FMLCommonSetupEvent evt) {
         compassConfig = createDefaultConfig();
         try {
-            compassConfig = GsonConfig.loadConfigWithDefault(CompassConfig.class, new File(Minecraft.getInstance().gameDir, "\\config\\findercompass.cfg"), compassConfig);
+            compassConfig = GsonConfig.loadConfigWithDefault(CompassConfig.class, new File(Minecraft.getInstance().gameDir, File.separator + "config" + File.separator + "findercompass.cfg"), compassConfig);
             loadSettingListFromConfig(compassConfig);
         } catch (IOException e) {
             LOGGER.error("IOException parsing config", e);
