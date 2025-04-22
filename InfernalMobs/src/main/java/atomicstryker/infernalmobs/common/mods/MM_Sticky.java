5bc7fd45b0b81b912059e7ecf7ca1e05df4206f2
AtomicStryker <atomicstrykermp@gmail.com>
2025-04-22 21:23:19 +0200

    Infernal Mobs 1.20.1.8, fix https://github.com/AtomicStryker/atomicstrykers-minecraft-mods/issues/541 in a better way

--- InfernalMobs/src/main/java/atomicstryker/infernalmobs/common/mods/MM_Sticky.java
+++ InfernalMobs/src/main/java/atomicstryker/infernalmobs/common/mods/MM_Sticky.java
@@ -39,15 +39,16 @@ public class MM_Sticky extends MobModifier {
             long time = System.currentTimeMillis();
             if (time > nextAbilityUse) {
                 nextAbilityUse = time + coolDown;
-                ItemStack droppedStack = p.getInventory().removeItem(p.getInventory().selected, 1);
-                ItemEntity drop = p.drop(droppedStack, false);
-                if (drop != null) {
-                    drop.setPickUpDelay(50);
-                    mob.level().playSound(null, mob.blockPosition(), SoundEvents.SLIME_ATTACK, SoundSource.HOSTILE, 1.0F + mob.getRandom().nextFloat(), mob.getRandom().nextFloat() * 0.7F + 0.3F);
-                } else {
-                    // drop was cancelled by forge hook, restore the item
-                    // see https://github.com/AtomicStryker/atomicstrykers-minecraft-mods/issues/541
-                    p.getInventory().add(droppedStack);
+                ItemStack equippedStack = p.getMainHandItem();
+                if (ItemStack.EMPTY != equippedStack) {
+                    // drop may be cancelled by forge event hook
+                    ItemEntity drop = p.drop(equippedStack, false);
+                    if (drop != null) {
+                        // assert successful drop to actually remove the item from inventory
+                        p.getInventory().removeFromSelected(false);
+                        drop.setPickUpDelay(50);
+                        mob.level().playSound(null, mob.blockPosition(), SoundEvents.SLIME_ATTACK, SoundSource.HOSTILE, 1.0F + mob.getRandom().nextFloat(), mob.getRandom().nextFloat() * 0.7F + 0.3F);
+                    }
                 }
             }
         }
