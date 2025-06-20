ed193ab049358668f7c33849e321824c8cdd6e76
AtomicStryker <atomicstrykermp@gmail.com>
2025-06-20 11:42:48 +0200

    Dynamic Lights 1.21.6.1 for MC 1.21.6 and Forge 58.0.3 with EventBus 7

--- DynamicLights/src/main/java/atomicstryker/dynamiclights/server/modules/PlayerSelfLightSource.java
+++ DynamicLights/src/main/java/atomicstryker/dynamiclights/server/modules/PlayerSelfLightSource.java
@@ -16,7 +16,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.server.ServerAboutToStartEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
