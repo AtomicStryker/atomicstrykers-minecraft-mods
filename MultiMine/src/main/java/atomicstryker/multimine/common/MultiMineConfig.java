713e5bfa907273b765ef5d1ab7f1f7aaf2fb8178
CreativeMD <Creative-Multimedia-Design@gmx.de>
2025-11-13 09:28:34 -0400

    Added littletiles combat to default banned blocks config (#556)

--- MultiMine/src/main/java/atomicstryker/multimine/common/MultiMineConfig.java
+++ MultiMine/src/main/java/atomicstryker/multimine/common/MultiMineConfig.java
@@ -14,6 +14,14 @@ public class MultiMineConfig {
     private boolean disableForAllTileEntities = false;
     private boolean disableAutoRegisterNames = false;
 
+    public MultiMineConfig() {
+        // Default banned blocks added due to https://github.com/AtomicStryker/atomicstrykers-minecraft-mods/issues/555
+        this.bannedBlocks.put("littletiles:tiles", true);
+        this.bannedBlocks.put("littletiles:tiles_ticking", true);
+        this.bannedBlocks.put("littletiles:tiles_rendered", true);
+        this.bannedBlocks.put("littletiles:tiles_ticking_rendered", true);
+    }
+
     public boolean isBlockRegenerationEnabled() {
         return blockRegenerationEnabled;
     }
