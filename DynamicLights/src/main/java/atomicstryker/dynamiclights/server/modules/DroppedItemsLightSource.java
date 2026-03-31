6c29e1101db85c42b7ee3a99711575f052d28c99
AtomicStryker <atomicstrykermp@gmail.com>
2026-03-31 21:42:24 +0200

    Dynamic Lights 26.1.1NF

--- DynamicLights/src/main/java/atomicstryker/dynamiclights/server/modules/DroppedItemsLightSource.java
+++ DynamicLights/src/main/java/atomicstryker/dynamiclights/server/modules/DroppedItemsLightSource.java
@@ -135,7 +135,7 @@ public class DroppedItemsLightSource {
             lightLevel = 0;
             enabled = false;
             entity = eI;
-            notWaterProof = notWaterProofItems.getLightLevel(eI.getItem(), eI.registryAccess()) > 0 || eI.getItem().getTags().anyMatch(rl -> rl.location().equals(DynamicLights.NOT_WATERPROOF_TAG));
+            notWaterProof = notWaterProofItems.getLightLevel(eI.getItem(), eI.registryAccess()) > 0 || eI.getItem().tags().anyMatch(rl -> rl.location().equals(DynamicLights.NOT_WATERPROOF_TAG));
         }
 
         /**
